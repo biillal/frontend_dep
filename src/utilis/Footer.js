ba8171494ae630483049a3b034c11ff369c3413a
billal <billal@gmail.com>
2024-07-17 23:32:02 +0200

    upd3 FOOTER

--- src/utilis/Footer.js
+++ src/utilis/Footer.js
@@ -4,7 +4,7 @@ import React from 'react'
 function Footer() {
   return (
     <div className='bg-blue-900 p-4 mt-12'>
-      <Text className='text-center text-xl text-white font-bold'>Laboratory of Materials Chemistry,Catalysis and Reactivity  | ICCSM@2024 | Faculté des sciences exactes et l'informatique département Chimie Univerity  of Chlef</Text>
+      <Text className='text-center text-xl text-white font-bold'>faculty of exact sciences and computer science, department of chemistry, university of Chlef</Text>
     </div>
   )
 }

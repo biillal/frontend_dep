d33fcab5b1ec2ea3cbf137ba2bf7dd2149e3fdee
billal <billal@gmail.com>
2024-07-17 22:42:35 +0200

    upd2 footer

--- src/utilis/Footer.js
+++ src/utilis/Footer.js
@@ -4,7 +4,7 @@ import React from 'react'
 function Footer() {
   return (
     <div className='bg-blue-900 p-4 mt-12'>
-      <Text className='text-center text-xl text-white font-bold'>Physico-Chemistry Laboratory Of Materials Chemistry , Catelysis and Reactivity | ICMSE@2024 | Copyright © 2024 | All rights rederved</Text>
+      <Text className='text-center text-xl text-white font-bold'>Laboratory of Materials Chemistry,Catalysis and Reactivity  | ICCSM@2024 | Faculté des sciences exactes et l'informatique département Chimie Univerity  of Chlef</Text>
     </div>
   )
 }
